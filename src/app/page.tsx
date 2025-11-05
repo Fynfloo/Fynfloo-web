ccb37c43677c22140397028e5dfc0ba422196b34
Mike <michaelokolo62@yahoo.com>
2025-11-05 06:29:29 +0000

    Solve hero deployement problem

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 'use client';
 
-import { useAuth } from '../context/AuthContext';
 import { Navbar } from '@/components/navbar';
-import { Hero } from '@/components/hero';
+import { Hero } from '@/components/Hero';
 
 export default function Home() {
-  const { user, logout } = useAuth();
   return (
     <main style={{ margin: '0rem auto' }}>
       <Navbar />
---
+++ src/components/bento-grid.tsx
@@ -0,0 +1,12 @@
+// import { Card, CardContent } from '@/components/Card';
+
+interface Step {
+  title: string;
+  description: string;
+  icon?: React.ReactNode;
+  accent?: string;
+}
+
+const BentoGrid = () => {
+  return <div></div>;
+};
