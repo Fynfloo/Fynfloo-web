099b94cabe023340aa809c29e46f7fc32e41bd18
Mike <michaelokolo62@yahoo.com>
2025-11-12 03:15:57 +0000

    set up editable

--- src/components/craft/text.tsx
+++ src/components/craft/text.tsx
@@ -1,28 +1,53 @@
 import * as React from 'react';
+import { useEffect } from 'react';
 import { useNode } from '@craftjs/core';
+import ContentEditable from 'react-contenteditable';
+import {
+  CraftComponent,
+  ContentEditableEvent,
+} from '../../types/craft-component';
 
 interface TextProps {
   text: string;
   fontSize?: string | number;
 }
 
-export const Text = ({
-  text,
-  fontSize = 20,
-}: {
-  text: string;
-  fontSize?: string | number;
-}) => {
+export const Text: CraftComponent<TextProps> = ({ text, fontSize = 20 }) => {
   const {
     connectors: { connect, drag },
-  } = useNode();
+    hasSelectedNode,
+    hasDraggedNode,
+    actions: { setProp },
+  } = useNode((state) => ({
+    hasSelectedNode: state.events.selected,
+    hasDraggedNode: state.events.dragged,
+  }));
+
+  const [editable, setEditable] = React.useState(false);
+
+  useEffect(() => {
+    !hasSelectedNode && setEditable(false);
+  }, [hasSelectedNode]);
+
   return (
     <div
       ref={(ref) => {
         if (ref) connect(drag(ref));
       }}
+      onClick={(e) => setEditable(true)}
     >
-      <p style={{ fontSize }}>{text}</p>
+      <ContentEditable
+        disabled={!editable}
+        html={text}
+        onChange={(e: ContentEditableEvent) => {
+          const value = e.target.value.replace(/<\/?[^>]+(>|$)/g, '');
+          setProp((props: TextProps) => {
+            props.text = value;
+          });
+        }}
+        tagName="p"
+        style={{ fontSize, margin: 0, padding: 0 }}
+      />
     </div>
   );
 };
---
+++ src/types/craft-component.ts
@@ -0,0 +1,15 @@
+export type CraftComponent<TProps extends object = Record<string, unknown>> =
+  React.FC<TProps> & {
+    craft?: {
+      rules?: {
+        canDrag?: (node: Node & { data: { props: TProps } }) => boolean;
+        canMoveIn?: (incomingNodes: Node[]) => boolean;
+        canMoveOut?: (outgoingNodes: Node[]) => boolean;
+      };
+      related?: Record<string, React.ComponentType<any>>;
+    };
+  };
+
+export type ContentEditableEvent = {
+  target: EventTarget & { value: string };
+};
