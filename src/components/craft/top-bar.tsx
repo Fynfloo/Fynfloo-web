47410a0bc475acf24a1d584bd8956605d093e50d
Mike <michaelokolo62@yahoo.com>
2025-11-12 23:53:37 +0000

    add the serializer functionality

--- src/components/craft/top-bar.tsx
+++ src/components/craft/top-bar.tsx
@@ -1,18 +1,36 @@
 import { Label } from '@radix-ui/react-label';
 // import { Switch } from '@radix-ui/react-switch';
 import { Box, Button, Flex, Switch } from '@radix-ui/themes';
+import { useEditor } from '@craftjs/core';
 
 export const TopBar = () => {
+  const { actions, query, enabled } = useEditor((state) => ({
+    enabled: state.options.enabled,
+  }));
   return (
     <Box px="1" py="1" mt="3" mb="1">
       <Box>
         <Flex as="div" align="center" justify="between">
           <Flex as="div" align="center" justify="center" gap="1">
-            <Switch id="enable" value="true" defaultChecked />
+            <Switch
+              id="enable"
+              checked={enabled}
+              onCheckedChange={(value) =>
+                actions.setOptions((options) => {
+                  options.enabled = value;
+                })
+              }
+            />
             <Label htmlFor="enable">Enable</Label>
           </Flex>
           <Flex as="div" align="center" justify="center">
-            <Button size="2" variant="outline">
+            <Button
+              size="2"
+              variant="outline"
+              onClick={() => {
+                console.log(query.serialize());
+              }}
+            >
               Serialize JSON to console
             </Button>
           </Flex>
